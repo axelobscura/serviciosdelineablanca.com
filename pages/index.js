ddc573d0af2904c3310035261d2b386f59b9f6b3
Axel Obscura <axosar@gmail.com>
2020-10-11 16:04:32 -0500

    titulo dos

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+  const masInfo = (e) => {
+    console.log('amigo');
+  }
+
+  const masContacto = (e) => {
+    console.log('amigo');
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -16,11 +27,22 @@ export default function Home() {
         <h2>(55)6235-3054</h2>
         <h3>CDMX, Estado de México, Toluca, Metepec, Lerma</h3>
         <p className="center-text" style={{ textAlign: 'center' }}><b>Contamos con técnicos capacitados</b><br />Somos el servicio de linea blanca en donde atendemos todos sus requerimientos<br />Reparación, Servicio de Mantenimiento e instalación de lavadoras, refrigeradores, secadoras, centros de lavado, lavavajillas, estufas y hornos de gas.</p>
-        <div class="btn-group btn-group-toggle" data-toggle="buttons">
-          <button type="button" className="btn btn-primary mr-2">MÁS INFORMACIÓN</button>
-          <button type="button" className="btn btn-primary">PÓNGASE EN CONTACTO</button>
+        <div className="btn-group btn-group-toggle" data-toggle="buttons">
+          <button type="button" className="btn btn-primary mr-2" onClick={masInfo}>MÁS INFORMACIÓN</button>
+          <button type="button" className="btn btn-primary" onClick={masContacto}>PÓNGASE EN CONTACTO</button>
         </div>
       </main>
+
+      <main className={styles.mainDos} id="masInfo">
+        <h2>Reparación de lavadoras</h2>
+        <hr />
+        <p className="center-text" style={{ textAlign: 'center' }}>¿Tú lavadora ya no lava cómo antes? ¿se mueve de lugar a lugar a la hora de prender la lavadora? Será mejor que llame a un técnico para evitar un problema mayor, por lo que lo invitamos a ponerse en contacto con nosotros para ofrecerle un servicio de excelencia.</p>
+        <div className="btn-group btn-group-toggle" data-toggle="buttons">
+          <button type="button" className="btn btn-primary mr-2" onClick={masInfo}>MÁS INFORMACIÓN</button>
+          <button type="button" className="btn btn-primary" onClick={masContacto}>PÓNGASE EN CONTACTO</button>
+        </div>
+      </main>
+
       <footer className={styles.footer} style={{ textAlign: 'center', flexDirection: 'column', padding: '0' }}>
         <b>SERVICIOS DE LÍNEA BLANCA</b>CDMX, Estado de México, Toluca, Metepec, Lerma<br />(55)6235-3054
       </footer>
