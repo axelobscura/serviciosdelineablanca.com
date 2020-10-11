e0dff0582851a004287f60d0ba0bf93633865785
Axel Obscura <axosar@gmail.com>
2020-10-11 11:58:28 -0500

    fondo de slide

--- pages/index.js
+++ pages/index.js
@@ -10,13 +10,14 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          SERVICIOS DE LÍNEA BLANCA
-        </h1>
+        <h1 className={styles.title}>SERVICIOS DE LÍNEA BLANCA</h1>
+        <h2>(55)6235-3054</h2>
+        <h3>CDMX, Estado de México, Toluca, Metepec, Lerma</h3>
+        <p className="center-text" style={{ textAlign: 'center' }}><b>Contamos con técnicos capacitados</b><br />Somos el servicio de linea blanca en donde atendemos todos sus requerimientos</p>
       </main>
 
-      <footer className={styles.footer}>
-          SERVICIOS DE LÍNEA BLANCA EN MÉXICO
+      <footer className={styles.footer} style={{ textAlign: 'center', flexDirection: 'column', padding: '0' }}>
+        <b>SERVICIOS DE LÍNEA BLANCA</b>CDMX, Estado de México, Toluca, Metepec, Lerma
       </footer>
     </div>
   )
