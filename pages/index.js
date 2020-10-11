bc66fb4cc98f4b7e1883b25768425e152753b8c7
Axel Obscura <axosar@gmail.com>
2020-10-11 12:06:50 -0500

    fondo de slide

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,11 @@ export default function Home() {
         <h1 className={styles.title}>SERVICIOS DE LÍNEA BLANCA</h1>
         <h2>(55)6235-3054</h2>
         <h3>CDMX, Estado de México, Toluca, Metepec, Lerma</h3>
-        <p className="center-text" style={{ textAlign: 'center' }}><b>Contamos con técnicos capacitados</b><br />Somos el servicio de linea blanca en donde atendemos todos sus requerimientos</p>
+        <p className="center-text" style={{ textAlign: 'center' }}><b>Contamos con técnicos capacitados</b><br />Somos el servicio de linea blanca en donde atendemos todos sus requerimientos<br />Reparación, Servicio de Mantenimiento e instalación de lavadoras, refrigeradores, secadoras, centros de lavado, lavavajillas, estufas y hornos de gas.</p>
       </main>
 
       <footer className={styles.footer} style={{ textAlign: 'center', flexDirection: 'column', padding: '0' }}>
-        <b>SERVICIOS DE LÍNEA BLANCA</b>CDMX, Estado de México, Toluca, Metepec, Lerma
+        <b>SERVICIOS DE LÍNEA BLANCA</b>CDMX, Estado de México, Toluca, Metepec, Lerma<br />(55)6235-3054
       </footer>
     </div>
   )
