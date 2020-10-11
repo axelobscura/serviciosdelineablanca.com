c0d7a06e57392ecc0dfc2ba9c2aea5566de35ce3
Axel Obscura <axosar@gmail.com>
2020-10-11 15:16:24 -0500

    Whatsapp button

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import '../styles/globals.css'
 
+if (typeof window !== 'undefined') {
+  (function () {
+    var options = {
+      whatsapp: "525562353054", // WhatsApp number
+      call_to_action: "Envíanos un mensaje", // Call to action
+      position: "right", // Position may be 'right' or 'left'
+    };
+    var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+    var s = document.createElement('script');
+    s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+    s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+  })();
+}
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,20 @@ export default function Home() {
       <Head>
         <title>SERVICIOS DE LÍNEA BLANCA EN MÉXICO</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/lux/bootstrap.min.css" />
       </Head>
 
       <main className={styles.main}>
+        <img src="https://centrosautorizadoscdmx.com/img/logo.png" alt="SERVICIOS DE LÍNEA BLANCA EN MÉXICO" title="SERVICIOS DE LÍNEA BLANCA EN MÉXICO" style={{ width: '200px', background: 'rgba(255,255,255,0.8)', padding: '10px', borderRadius: '30px' }} />
         <h1 className={styles.title}>SERVICIOS DE LÍNEA BLANCA</h1>
         <h2>(55)6235-3054</h2>
         <h3>CDMX, Estado de México, Toluca, Metepec, Lerma</h3>
         <p className="center-text" style={{ textAlign: 'center' }}><b>Contamos con técnicos capacitados</b><br />Somos el servicio de linea blanca en donde atendemos todos sus requerimientos<br />Reparación, Servicio de Mantenimiento e instalación de lavadoras, refrigeradores, secadoras, centros de lavado, lavavajillas, estufas y hornos de gas.</p>
+        <div class="btn-group btn-group-toggle" data-toggle="buttons">
+          <button type="button" className="btn btn-primary mr-2">MÁS INFORMACIÓN</button>
+          <button type="button" className="btn btn-primary">PÓNGASE EN CONTACTO</button>
+        </div>
+
       </main>
 
       <footer className={styles.footer} style={{ textAlign: 'center', flexDirection: 'column', padding: '0' }}>
