83dd7a0fdcc9b48755d00d76ccad7ed6612d0ca5
axelobscura <axosar@gmail.com>
2020-10-15 21:19:31 -0500

    Logotipo

--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <img src="https://centrosautorizadoscdmx.com/img/logo.png" alt="SERVICIOS DE LÍNEA BLANCA EN MÉXICO" title="SERVICIOS DE LÍNEA BLANCA EN MÉXICO" style={{ width: '200px', background: 'rgba(255,255,255,0.8)', padding: '10px', borderRadius: '30px' }} />
-        <h1 className={styles.title}>SERVICIOS DE LÍNEA BLANCA</h1>
+        <h1 className={styles.title}>SERVICIOS ESPECIALIZADOS<br/>DE LÍNEA BLANCA</h1>
         <h2>(55)6235-3054</h2>
         <h3>CDMX, Estado de México, Toluca, Metepec, Lerma</h3>
         <p className="center-text" style={{ textAlign: 'center' }}><b>Contamos con técnicos capacitados</b><br />Somos el servicio de linea blanca en donde atendemos todos sus requerimientos<br />Reparación, Servicio de Mantenimiento e instalación de lavadoras, refrigeradores, secadoras, centros de lavado, lavavajillas, estufas y hornos de gas.</p>
